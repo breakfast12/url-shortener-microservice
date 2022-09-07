227664ffb48780aab75979b0ab47ccc71288e0c4
fresaridho <freza.ridho@roketin.com>
2022-09-07 11:41:14 +0700

    post url shorterner

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const shortId = require('shortid');
 const app = express();
 
 // Basic Configuration
@@ -9,6 +12,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -17,6 +22,41 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// URL Shortener Microservice
+const connection = mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: String
+});
+
+const Url = mongoose.model('Url', urlSchema);
+
+app.post('/api/shorturl', (req, res) => {
+  var url = req.body.url;
+  var urlGenerate = shortId.generate();
+
+  var postURL = new Url({
+    original_url: url,
+    short_url: urlGenerate
+  });
+
+  postURL.save((err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    done(null, data)
+  });
+
+  res.json({ 
+    original_url: url, 
+    short_url: urlGenerate
+  });
 });
 
 app.listen(port, function() {
